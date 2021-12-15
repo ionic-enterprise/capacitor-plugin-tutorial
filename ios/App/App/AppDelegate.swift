1705ea3c8aa56c5165520fd17568f7e426ad355d
Eric Horodyski <horodyski@ionic.io>
2021-12-15 16:03:26 -0500

    step(4): the ios implementation

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -5,6 +5,10 @@ import Capacitor
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+  
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+      return ScreenOrientationPlugin.supportedOrientations
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
