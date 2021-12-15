c477f08e92ac49cc9c27d8741b6e8538a3039e64
Eric Horodyski <horodyski@ionic.io>
2021-12-15 15:43:33 -0500

    step(3): web/pwa implementation

---
+++ src/plugins/screen-orientation/web.ts
@@ -0,0 +1,27 @@
+import { WebPlugin } from "@capacitor/core";
+import type { ScreenOrientationPlugin } from "./definitions";
+
+export class ScreenOrientationWeb
+  extends WebPlugin
+  implements ScreenOrientationPlugin
+{
+  constructor() {
+    super();
+    window.screen.orientation.addEventListener("change", () => {
+      const type = window.screen.orientation.type;
+      this.notifyListeners("screenOrientationChange", { type });
+    });
+  }
+
+  async orientation(): Promise<{ type: OrientationType }> {
+    return { type: window.screen.orientation.type };
+  }
+
+  async lock(opts: { orientation: OrientationLockType }): Promise<void> {
+    await window.screen.orientation.lock(opts.orientation);
+  }
+
+  async unlock(): Promise<void> {
+    window.screen.orientation.unlock();
+  }
+}
