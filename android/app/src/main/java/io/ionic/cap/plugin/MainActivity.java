9bbef8c79d0613d9aa76b675c7ada5b34983b20c
Eric Horodyski <horodyski@ionic.io>
2021-12-16 11:34:43 -0500

    step(5): the android implementation

--- android/app/src/main/java/io/ionic/cap/plugin/MainActivity.java
+++ android/app/src/main/java/io/ionic/cap/plugin/MainActivity.java
@@ -1,5 +1,15 @@
 package io.ionic.cap.plugin;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import io.ionic.cap.plugin.plugins.ScreenOrientation.ScreenOrientationPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(ScreenOrientationPlugin.class);
+    }
+
+}
