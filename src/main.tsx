43b1829c965188251613627154d5e24e55412942
Shiva Prasad <sp@shiv19.com>
2024-05-26 13:11:15 +1200

    chore: update start template to capacitor 6

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
